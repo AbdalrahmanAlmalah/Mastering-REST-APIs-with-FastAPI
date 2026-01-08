0da0f3af79ff6508ffd46c8672e74c46e700d2ec
Abdalrahman Almalah <abdulrahmanalmallah@gmail.com>
2026-01-08 14:58:00 +0300

    addind ruff, create post model

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 
-app=FastAPI()
+from storeapi.models.post import UserPost, UserPostIn
 
-## decoretor to make the function run when calling the api
-@app.get("/")
-## async means that the function can run more-less then the times that the functions do
-async def root():
-    return {"message": "Welecome to your First API in FastAPI"}
+app = FastAPI()
+
+
+post_tabel = {}
+
+
+@app.post("/", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(post_tabel)
+    new_post = {**data, "id": last_record_id}
+    post_tabel[last_record_id] = new_post
+    return new_post
+
+
+@app.get("/", response_model=list[UserPost])
+async def get_posts():
+    return list(post_tabel.values())
