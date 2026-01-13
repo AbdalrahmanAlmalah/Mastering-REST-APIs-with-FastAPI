ef1a0f4656dfcbc26e5c172213e4e2259edbc742
Abdalrahman Almalah <abdulrahmanalmallah@gmail.com>
2026-01-13 22:39:24 +0300

    Adding Routers

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,22 +1,6 @@
 from fastapi import FastAPI
-
-from storeapi.models.post import UserPost, UserPostIn
+from routers.post import router as post_router
 
 app = FastAPI()
-
-
+app.include_router(post_router, prefix="/api")
 post_tabel = {}
-
-
-@app.post("/", response_model=UserPost)
-async def create_post(post: UserPostIn):
-    data = post.dict()
-    last_record_id = len(post_tabel)
-    new_post = {**data, "id": last_record_id}
-    post_tabel[last_record_id] = new_post
-    return new_post
-
-
-@app.get("/", response_model=list[UserPost])
-async def get_posts():
-    return list(post_tabel.values())
